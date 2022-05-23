eee9266e66992e9ff7395ba568a4915f2c0567b8
atovmasan1 <yt988998@gmail.com>
2022-05-23 10:04:44 +0300

    Host version 5

---
+++ precache-manifest.6713127c0d7bc8da84b7e0e13c0400d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "64217083a12d0920a253",
+    "url": "/VPIGame/static/css/main.a5b0a175.chunk.css"
+  },
+  {
+    "revision": "64217083a12d0920a253",
+    "url": "/VPIGame/static/js/main.7e7bee70.chunk.js"
+  },
+  {
+    "revision": "4c4c787deeedce9b28c4",
+    "url": "/VPIGame/static/js/runtime~main.c2ddaceb.js"
+  },
+  {
+    "revision": "1aec2b8ad6255dbb18be",
+    "url": "/VPIGame/static/css/2.000d15e8.chunk.css"
+  },
+  {
+    "revision": "1aec2b8ad6255dbb18be",
+    "url": "/VPIGame/static/js/2.04be4d0a.chunk.js"
+  },
+  {
+    "revision": "0e997d60b524bf9804f7",
+    "url": "/VPIGame/static/js/3.af1b0541.chunk.js"
+  },
+  {
+    "revision": "ad9ee4215af40595c43547371306f4bc",
+    "url": "/VPIGame/index.html"
+  }
+];
