566b7c6d24baeecb6847a6cb34b0beb583f8bbc8
atovmasan <yt988998@gmail.com>
2022-06-11 12:37:46 +0300

    Debug

--- src/DAL/NewParametr.js
+++ src/DAL/NewParametr.js
@@ -5,7 +5,7 @@ import { GiveExtraDataActionCreator } from "../Redux/ContentReducer"
 export const NewParametrThunkCreator = (id, name) => {
     return async function Parametr(dispatch, getState) {
         let data = getState().ContentReducer.CountryData
-        data.CountriesTruth[id].LastElementID = data.CountriesTruth[id].LastElementID + 1
+        data.CountriesTruth[id].LastElementID = data.CountriesTruth[id].LastElementID
         data.CountriesTruth[id].Economy.push({
             coffers: {
                 count: 1,
